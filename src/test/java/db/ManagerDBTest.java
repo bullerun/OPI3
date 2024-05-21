2e2e6713100aad9412ebf4bf93590e34d4a0d695
bullerun1 <ryskalnikita2004@mail.ru>
2024-05-21 09:17:32 +0300

    Add test reports

---
+++ src/test/java/db/ManagerDBTest.java
@@ -0,0 +1,28 @@
+package db;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ManagerDBTest {
+    Point point;
+
+    @BeforeEach
+    public void testBefore() {
+        point = new Point(1, 2, 3);
+    }
+
+    @Test
+    public void test() {
+        assertEquals(1, point.getX());
+        point.setX(3);
+        System.out.println("как то-так");
+        assertEquals(3,point.getX(), " ");
+    }
+}
